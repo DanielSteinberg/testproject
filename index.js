35ab471709e2075f705ae2f96dd95f6098d0040f
Daniel <daniel.steinberg222@gmail.com>
2017-11-22 18:14:17 +0300

    var http = require('http');

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 const PORT=8080; 
 
-fs.readFile('./index.html', function (err, html) {
+fs.readFile('./main.html', function (err, html) {
 
     if (err) throw err;    
 
@@ -12,4 +12,28 @@ fs.readFile('./index.html', function (err, html) {
         response.write(html);  
         response.end();  
     }).listen(PORT);
+
+const PORT2=8080/main; 
+
+fs.readFile('./main.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT2);
+
+const PORT3=8080/notes; 
+
+fs.readFile('./notes.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(PORT3);
 });
