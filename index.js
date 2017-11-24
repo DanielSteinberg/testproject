7369749ea5b8b3e15345e65757f85a3c89326300
Daniel <daniel.steinberg222@gmail.com>
2017-11-24 15:16:56 +0300

    Routing changes

--- index.js
+++ index.js
@@ -1,39 +1,6 @@
 var http = require('http');
-var fs = require('fs');
+var app = require('./route');
 
 const PORT=8080; 
 
-fs.readFile('./main.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-
-const PORT2=8080/main; 
-
-fs.readFile('./main.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT2);
-
-const PORT3=8080/notes; 
-
-fs.readFile('./notes.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT3);
-});
+http.createServer(app.handleRequest).listen(PORT);
